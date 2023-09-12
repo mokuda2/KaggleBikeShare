2122e77757d59011d31a7f9470c04f998aa5c63a
mokuda2 <michael.okuda99@gmail.com>
2023-09-11 23:05:33 -0600

    plots

--- BikeShareEDA.R
+++ BikeShareEDA.R
@@ -2,5 +2,19 @@
 
 library(tidyverse)
 library(vroom)
+library(DataExplorer)
 
 # Read in the data
+bike <- vroom("./STAT\ 348/KaggleBikeShare/train.csv")
+bike
+
+plot_intro(bike)
+dplyr::glimpse(bike)
+DataExplorer::plot_correlation(bike)
+DataExplorer::plot_bar(bike)
+
+# registered and casual and count are right skewed
+DataExplorer::plot_histogram(bike)
+
+# no missing data
+DataExplorer::plot_missing(bike)
